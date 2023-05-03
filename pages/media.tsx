3d5329d96e506a9b39c446d2d80d8d3837ee65b3
mcast2185 <76968385+mcast2185@users.noreply.github.com>
2023-05-03 02:59:25 -0600

    update

--- pages/media.tsx
+++ pages/media.tsx
@@ -1,5 +1,11 @@
-import { useEffect } from 'react';
 import Link from 'next/link';
+import { EffectCoverflow, Pagination, Navigation } from 'swiper';
+import { SwiperSlide, Swiper } from 'swiper/react';
+import React, { useState, useEffect } from 'react';
+import type { RadioChangeEvent } from 'antd';
+import { Carousel, Radio } from 'antd';
+import type { DotPosition } from 'antd/es/carousel';
+
 
 import Mouse from 'components/mouse';
 import Header from '../components/header';
@@ -15,10 +21,17 @@ import APEX from '../public/apex.png';
 import TED from '../public/ted.png';
 import "../styles/globals.css";
 import Head from 'next/head';
-import { Carousel } from 'antd';
+import Footer from 'components/footer';
 
 
-export default function Other () {
+
+export default function Media () {
+  const [dotPosition, setDotPosition] = useState<DotPosition>('top');
+
+  const handlePositionChange = ({ target: { value } }: RadioChangeEvent) => {
+    setDotPosition(value);
+  };
+
   useEffect(() => {
     document.getElementById("mainImg")?.addEventListener("mouseenter", ()=> {
       document.getElementById("mando")!.style.filter = "brightness(.6)";
@@ -108,48 +121,158 @@ export default function Other () {
               sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation">
             </iframe>
           </div>
-          <div className="">
-             <Carousel autoplay effect="fade">
-              <div>
-                <Image src={GALAXY} width={480} height={220} alt="Guardians of the Galaxy 3 movie promo"/>
-                <h3 className="text-center h-28 text-slate-50 bg-[#3c3c3c] bg-opacity-20 leading-[100px]">1</h3>
+          <div className=" flex flex-row mb-10 w-full">
+            <div className="flex flex-col justify-start pr-5 ml-[60px] items-center w-[580px]" >
+              <Carousel autoplay autoplaySpeed={3000} effect="fade" className="w-[580px] h-[250px] mb-10">
+                <div className="flex justify-center pr-6 overflow-hidden w-[580px] h-[250px]">
+                  <Image src={GALAXY} width={580} height={250} alt="Guardians of the Galaxy 3 movie promo"/>
+                  <Link target="_blank" href="https://www.youtube.com/watch?v=hvfTZM4aNck" aria-details=''>
+                    <h3 className="font-bold p-2 rounded-sm bg-[#3c3c3c] hover:bg-transparent hover:scale-105 transition-transform 
+                      duration-200 ease-in-out bg-opacity-60 text-stone-50 mr-2 text-2xl font-Quicksand absolute mt-[150px]">
+                      Guardians of the Galaxy 3: Final trailer
+                    </h3>
+                  </Link>
+                </div>
+                <div className="flex justify-center pr-6 overflow-hidden w-[580px] h-[250px]">
+                  <Image src={KINGDOM} width={580} height={250} alt="The Legend of Zelda: Tears of the Kingdom"/>
+                  <Link href="" aria-details='The Legend of Zelda: Tears of the Kingdom'>
+                    <h3 className="font-bold p-2 rounded-sm bg-[#3c3c3c] hover:bg-transparent hover:scale-105 transition-transform 
+                      duration-200 ease-in-out bg-opacity-60 text-stone-50 mr-2 text-2xl font-Quicksand absolute mt-[150px]">
+                      "Zelda: Tears of the Kingdom" 
+                    </h3>
+                  </Link>
+                </div>
+                <div className="flex justify-center pr-6 overflow-hidden w-[580px] h-[250px]">
+                  <Image src={APEX} width={580} height={250} alt="Apex Legends Season 17: Arsenal"/>
+                  <Link href="" aria-details='Apex Legends Season 17: Arsenal'>
+                    <h3 className="font-bold p-2 rounded-sm bg-[#3c3c3c] hover:bg-transparent hover:scale-105 transition-transform 
+                      duration-200 ease-in-out bg-opacity-60 text-stone-50 mr-2 text-2xl font-Quicksand absolute mt-[150px]">
+                      Apex Legends: Season 17
+                    </h3>
+                  </Link>
+                </div>
+                <div className="flex justify-center pr-6 overflow-hidden w-[580px] h-[250px]">
+                  <Image src={TED} width={580} height={250} alt="Ted Lasso Season 3"/>
+                  <Link href="" aria-details='Ted Lasso Season 3'>
+                    <h3 className="font-bold p-2 rounded-sm bg-[#3c3c3c] hover:bg-transparent hover:scale-105 transition-transform 
+                      duration-200 ease-in-out bg-opacity-60 text-stone-50 mr-2 text-2xl font-Quicksand absolute mt-[150px]">
+                      Ted Lasso season 3
+                    </h3>
+                  </Link>
+                </div>
+              </Carousel>
+              <div className="container justify-start">
+                <Swiper
+                  effect={"coverflow"}
+                  grabCursor={true}
+                  centeredSlides={true}
+                  loop={true}
+                  slidesPerView={"auto"}
+                  coverflowEffect={{
+                    rotate: 0,
+                    stretch: 0,
+                    depth: 100,
+                    modifier: 2.5}}
+                    pagination={{el: ".swiper-pagination", clickable: true}}
+                    navigation={{
+                      nextEl: ".swiper-button-next",
+                      prevEl: ".swiper-button-prev",
+                      enabled: true
+                    }}
+                  modules={[EffectCoverflow, Pagination, Navigation]}
+                  className="swiper_container">
+
+                  <SwiperSlide>
+                    <div className="flex justify-center ">
+                      <iframe width="180" height="250" src="https://www.youtube.com/embed/kVe5UM_k1S0" title="YouTube video player"
+                        allow="accelerometer; play clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen={false}>
+                      </iframe>
+                    </div>
+                  </SwiperSlide>
+
+                  <SwiperSlide>
+                    <div className="flex justify-center">
+                      <iframe width="180" height="250" src="https://www.youtube.com/embed/F4FQETyGuWs" title="YouTube video player"
+                        allow="accelerometer; play clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen={false}>
+                      </iframe>         
+
+                    </div>
+                  </SwiperSlide>
+
+                  <SwiperSlide>
+                    <div className="flex justify-center">
+                      <iframe width="180" height="250" src="https://www.youtube.com/embed/fLmu19go2cs" title="YouTube video player"
+                        allow="accelerometer; play clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen={false}>
+                      </iframe>         
+
+                    </div>
+                  </SwiperSlide>
+                  
+                  <SwiperSlide>
+                    <div className="flex justify-center">
+                      <iframe width="180" height="250" src="https://www.youtube.com/embed/9AMm0NuiXOo" title="YouTube video player"
+                        allow="accelerometer; play clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen={false}>
+                      </iframe>      
+                  </div>
+                  </SwiperSlide>
+                  <SwiperSlide>
+                    <div className="flex justify-center">
+                      <iframe width="180" height="250" src="https://www.youtube.com/embed/64_wlwcQTfA" title="YouTube video player"
+                        allow="accelerometer; play clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen={false}>
+                      </iframe>          
+                    </div>
+                  </SwiperSlide>
+                  <SwiperSlide>
+                    <div className="flex justify-center">
+                      <iframe width="180" height="250" src="https://www.youtube.com/embed/mkCK0P1BXac" title="YouTube video player"
+                        allow="accelerometer; play clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen={false}>
+                      </iframe>          
+                    </div>
+                  </SwiperSlide>
+                  <div className="slider-controller">
+                    <div className="flex h-14 w-28 items-center content-center" >
+                      <div className="swiper-pagination"></div>
+                    </div>
+                  </div>
+                </Swiper>
               </div>
-              <div>
-                <Image src={KINGDOM} width={480} height={220} alt=""/>
-                <h3 className="text-center h-28 text-slate-50 bg-[#3c3c3c] bg-opacity-20 leading-[100px]">2</h3>
-              </div>
-              <div>
-                <Image src={APEX} width={480} height={220} alt=""/>
-                <h3 className="text-center h-28 text-slate-50 bg-[#3c3c3c] bg-opacity-20 leading-[100px]">3</h3>
-              </div>
-              <div>
-                <Image src={TED} width={480} height={220} alt=""/>
-                <h3 className="text-center h-28 text-slate-50 bg-[#3c3c3c] bg-opacity-20 leading-[100px]">4</h3>
-              </div>
-            </Carousel>
+            </div>
+            <div className="flex flex-col h-[605px]">
+              <Carousel autoplaySpeed={4500} dotPosition="left" effect="fade" className="w-[305px] h-[605px] mb-10">
+                <div className="flex flex-col justify-center mb-3 pr-2 pl-2 w-[305px] h-[605px] overflow-hidden" > 
+                  <div className="overflow-hidden h-[1500px]">
+                    <iframe sandbox="allow-modal" width="300" height="1500" src="https://www.tiktok.com/embed/7209055934377659694" title="YouTube video player" allowFullScreen={false} className="pb-20 overflow-hidden"
+                      allow="accelerometer; pause; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share;  allow-scripts; allow-top-navigation;">
+                    </iframe>
+                  </div> 
+                </div>
+                <div className="flex flex-col justify-center mb-3 pr-2 pl-2 w-[305px] h-[605px] overflow-hidden" > 
+                  <div className="overflow-hidden h-[1500px]">
+                    <iframe sandbox="allow-modal" width="300" height="1500" src="https://www.tiktok.com/embed/7173074436902473002" title="YouTube video player" allowFullScreen={false} className="pb-20 overflow-hidden"
+                      allow="accelerometer; pause; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share;  allow-scripts; allow-top-navigation;">
+                    </iframe>
+                  </div> 
+                </div>
+                <div className="flex flex-col justify-center mb-3 pr-2 pl-2 w-[305px] h-[605px] overflow-hidden" > 
+                  <div className="overflow-hidden h-[1500px]">
+                    <iframe sandbox="allow-modal" width="300" height="1500" src="https://www.tiktok.com/embed/7212371511603219754" title="YouTube video player" allowFullScreen={false} className="pb-20 overflow-hidden"
+                      allow="accelerometer; pause; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share;  allow-scripts; allow-top-navigation;">
+                    </iframe>
+                  </div> 
+                </div>
+                <div className="flex flex-col justify-center mb-3 pr-2 pl-2 w-[305px] h-[605px] overflow-hidden" > 
+                  <div className="overflow-hidden h-[1500px]">
+                    <iframe sandbox="allow-modal" width="300" height="1500" src="https://www.tiktok.com/embed/7085773186960313646" title="YouTube video player" allowFullScreen={false} className="pb-20 overflow-hidden"
+                      allow="accelerometer; pause; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share;  allow-scripts; allow-top-navigation;">
+                    </iframe>
+                  </div> 
+                </div>
+              </Carousel>
+            </div>
           </div>
-
-          {/* <div className="" >
-            <div className="flex flex-row justify-center h-[550px] items-center mt-5">
-              <div className="flex justify-center px-2" >
-                <iframe width="400" height="600" src="https://www.youtube.com/embed/kVe5UM_k1S0" title="YouTube video player"
-                  allow="accelerometer; play clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen={false}>
-                </iframe>
-              </div>
-
-              <div className="flex justify-center mb-5 pt-6 px-2 " > 
-                <iframe width="300" height="600" src="https://www.tiktok.com/embed/7209055934377659694" title="YouTube video player" allowFullScreen={false} className="overflow-hidden"
-                  allow="accelerometer; play; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share; allow-forms allow-same-origin; allow-scripts; allow-top-navigation;">
-                </iframe>
-              </div>
-            </div>
-            <div className="flex flex-row h-96 mt-10 mb-10 overflow-scroll" >
-            
-
-            </div>
-          </div> */}
         </div>
       </div>
+      <Footer />
     </div>
   )
 };
